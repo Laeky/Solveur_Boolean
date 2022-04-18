e20649e542d81175cc09ed017251a37002925aca
Laeky <33467433+Laeky@users.noreply.github.com>
2022-04-18 21:47:30 +0200

    Update Solveur.ml
    Added exemples and exo 2

--- Solveur.ml
+++ Solveur.ml
@@ -7,18 +7,7 @@ type eb =
   |XOR of eb * eb 
   |NAND of eb * eb 
   |NOT of eb;;
-
-(*let rec solveur exp =
-   match exp with
-   |Vrai -> Vrai
-   |Faux -> Faux
-   |NOT(a)->NOT(solveur a)
-   |AND(a,b)->AND((solveur a),(solveur b)) 
-   |NAND(a,b)->NAND((solveur a),(solveur b)) 
-   |OR(a,b)->OR((solveur a),(solveur b))
-   |XOR(a,b)->XOR((solveur a),(solveur b)) 
-(* not finished*) *)
-               
+(* prend une expression booléene en entrée et renvoie une liste de tout les variables ( y compris les doublons)*)
 let rec ens exp =
   match exp with 
   |Vrai -> []
@@ -29,27 +18,36 @@ let rec ens exp =
   |NAND(a,b)->(ens a)@(ens b)
   |OR(a,b)->(ens a)@(ens b)
   |XOR(a,b)->(ens a)@(ens b) 
-(* problème doublons dans la liste ,définir une fonction pour éliminer les doublons*)
 
 
-let rec appartient e l1 = 
-  match l1 with
+(*Fonction auxilière qu'on va utliser dans la fonction élim_doublon,
+permet de savoir si un element e appartient à la liste l*)
+let rec appartient e l = 
+  match l with
   |[] -> false
   |head::tail -> (e=head) || appartient e tail;;
 
-let rec elim_doublon l1 =
-  match l1 with
+(* définition d'une fonction pour éliminer les doublons, prend une liste l 
+et renvoie une liste sans doublons*)
+let rec elim_doublon l =
+  match l with
   |[]->[]
   |head::tail->
       if appartient head tail then
         elim_doublon tail
       else  head::elim_doublon tail ;;  
-  
-let rec append l1 l2 =
-  match l1 with
-  |[]->l2
-  |head::tail -> head :: append tail l2;;
 
+
+
+
+(* fonction auxilière append pour notre generateur d'environnements*)
+let append x ll = List.map ( fun l -> x::l) ll;;
+(* à commenter*)
+let rec generateur_aux l =
+  match l with
+    [] -> [[]]
+  | hd::tl ->let le = generateur_aux tl in
+      (append (hd,Faux) le)@(append (hd,Vrai) le);; 
 
 (**let rec gen_env l1 =
    match l1 with
@@ -57,34 +55,29 @@ let rec append l1 l2 =
 |head::tail->[(head,true),(head,false)]
 |head::tail -> gen_env head append gen_env tail;;
 **)
-(*Fonction permettant d'évaluer Les Variables*)
 
+(*Fonction permettant d'évaluer Les Variables*)
 
 let rec eval eb=
   match eb with 
   |Vrai -> Vrai
   |Faux -> Faux
-  |V(i)-> V(i)
+  |V(i)-> V(i)  (*redef*)
   |NOT eb -> NOT (eval eb)
   |AND (eb1,eb2) -> AND(eval eb1,eval eb2)
   |OR (eb1,eb2) -> OR(eval eb1,eval eb2)
   |XOR (eb1,eb2) -> XOR(eval eb1,eval eb2)
-  |NAND (eb1,eb2) -> NAND(eval eb1,eval eb2)
+  |NAND (eb1,eb2) -> NAND(eval eb1,eval eb2) 
 ;;
+(*
+  let rec eval lc(var,tf)=
+    match lc with
+      [[]]->
+*)
+(*On va définir les différents Operateurs ? -> inutile *)
 
-let append x ll = List.map ( fun l -> x::l) ll;;
+(*NON*)
 
-let rec generateur_aux l =
-  match l with
-    [] -> [[]]
-  | hd::tl ->let le = generateur_aux tl in
-      (append (a,FALSE) le)@(append (a,TRUE) le);; 
-
-let generation s = gene (variable2 s) ;;
-
-(*On va définir les différents Operateurs ? *)
-
-(*NON*) 
 (*let rec NON eb =
    match eb with
    |Vrai -> Faux
@@ -95,7 +88,8 @@ let generation s = gene (variable2 s) ;;
    |XOR(x,y)->OR(AND(NON(x),NON(y)),AND(x,y))
    |NAND(x,y)->AND(x,y);; 
 *)
-  (*OU*) 
+  (*OU*)
+
 (*let rec OR eb1 eb2 = 
    match eb1,eb2 with
    |eb1 = Faux,eb2 = Faux -> Faux
@@ -103,13 +97,15 @@ let generation s = gene (variable2 s) ;;
    |eb1 = Vrai,eb2 = Faux -> Vrai
    |eb1 = Vrai,eb2 = Vrai -> Vrai;; 
 (*OP_XOR*)
+  
  let XOR eb1 eb2 = 
    match eb1,eb2 with
    |eb1 = Faux,eb2 = Faux -> Faux
    |eb1 = Faux,eb2 = Vrai -> Vrai
    |eb1 = Vrai,eb2 = Faux -> Vrai
    |eb1 = Vrai,eb2 = Vrai -> Faux;;
-(*ER*)
+(*ET*)
+  
  let AND eb1 eb2 = 
    match eb1,eb2 with
    |eb1 = Faux,eb2 = Faux -> Faux
@@ -117,6 +113,7 @@ let generation s = gene (variable2 s) ;;
    |eb1 = Vrai,eb2 = Faux -> Faux
    |eb1 = Vrai,eb2 = Vrai -> Vrai;;
 (*OP_NAND*)
+  
  let NAND eb1 eb2 = 
    match eb1,eb2 with
    |eb1 = Faux,eb2 = Faux -> Vrai
@@ -131,3 +128,4 @@ let generation s = gene (variable2 s) ;;
 let formule = NOT(V 0);;
 let formule3 = AND(AND((V 1), NOT (V 2)),V 1);;
 elim_doublon(ens(formule3)) ;;
+generateur_aux(elim_doublon(ens(formule3)));;
